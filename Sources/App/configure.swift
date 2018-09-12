de55df2a83478fec646728bdd9238130c4164af7
UENO Kenichi <keno42@gmail.com>
2018-09-12 10:53:26 +0200

    init

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -3,6 +3,8 @@ import Vapor
 
 /// Called before your application initializes.
 public func configure(_ config: inout Config, _ env: inout Environment, _ services: inout Services) throws {
+    
+    
     /// Register providers first
     try services.register(FluentSQLiteProvider())
 
